50cf54f3577e5ac8bb781a9bd5e00acee64dcc9d
AinhoaOleaga <oleagayu@etud.insa-toulouse.fr>
2023-12-26 09:59:32 +0100

    corrected another error

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from projects import views
 
 urlpatterns = [
-    path("", views.prokect_index, name="project_index"),
+    path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"), #Dynamic creation of urls using the pk of each projects
-    
+
 ]
